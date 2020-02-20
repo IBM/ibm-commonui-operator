63884065b1357458dc6a0f484ab24fe21a965479
erica brown <ericabr@us.ibm.com>
2020-02-20 13:22:17 -0500

    Push CSV didnt have th correct op name

--- common/scripts/push-csv.sh
+++ common/scripts/push-csv.sh
@@ -17,8 +17,8 @@
 
 set -e
 QUAY_NAMESPACE=${QUAY_NAMESPACE:-opencloudio}
-QUAY_REPOSITORY=${QUAY_REPOSITORY:-ibm-metering-operator-app}
-BUNDLE_DIR=${BUNDLE_DIR:-deploy/olm-catalog/ibm-metering-operator}
+QUAY_REPOSITORY=${QUAY_REPOSITORY:-ibm-commonui-operator-app}
+BUNDLE_DIR=${BUNDLE_DIR:-deploy/olm-catalog/ibm-commonui-operator}
 
 [[ "X$QUAY_USERNAME" == "X" ]] && read -rp "Enter username quay.io: " QUAY_USERNAME
 [[ "X$QUAY_PASSWORD" == "X" ]] && read -rsp "Enter password quay.io: " QUAY_PASSWORD && echo
