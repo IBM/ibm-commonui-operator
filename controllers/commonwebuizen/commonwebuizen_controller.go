cbdc68b7476c7b25346759d2074aa468899ca68f
Steve Grube <sgrube@us.ibm.com>
2022-04-29 03:16:02 -0400

    handle conditions where new env vars did not exist on upgrade (#304)
    * handle conditions where new env vars did not exist on upgrade
    
    * replace slice index function that makes linter crazy

--- controllers/commonwebuizen/commonwebuizen_controller.go
+++ controllers/commonwebuizen/commonwebuizen_controller.go
@@ -354,52 +354,72 @@ func (r *CommonWebUIZenReconciler) updateCommonUIDeployment(ctx context.Context,
 
 	if getError == nil {
 		reqLogger.Info("Got Common UI deployment")
-		env := commonDeployment.Spec.Template.Spec.Containers[0].Env[22].Value
-		clusterType := "cncf"
-		if isZen && env == "false" {
-			reqLogger.Info("Setting use zen to true")
-			commonDeployment.Spec.Template.Spec.Containers[0].Env[22].Value = "true"
-			if isCncf {
-				reqLogger.Info("Setting cluster type env var to cncf for zen case")
-				envLength := len(commonDeployment.Spec.Template.Spec.Containers[0].Env)
-				if envLength > 24 {
-					clusterTypeEnvVar := commonDeployment.Spec.Template.Spec.Containers[0].Env[24].Value
-					if clusterTypeEnvVar != clusterType {
-						commonDeployment.Spec.Template.Spec.Containers[0].Env[24].Value = clusterType
-					}
-				}
+
+		needUpdate := false
+
+		isZenStr := "false"
+		if isZen {
+			isZenStr = "true"
+		}
+
+		clusterTypeStr := "unknown"
+		if isCncf {
+			clusterTypeStr = "cncf"
+		}
+
+		//Check for env vars that were added and add if necessary (this is quasi migration code from 3.6 EUS forward)
+		// The variables are referred to positionally so they must get added in the correct order
+
+		isZenIdx := -1
+		for i := range commonDeployment.Spec.Template.Spec.Containers[0].Env {
+			if commonDeployment.Spec.Template.Spec.Containers[0].Env[i].Name == "USE_ZEN" {
+				isZenIdx = i
+				break
 			}
+		}
+		if isZenIdx < 0 {
+			commonDeployment.Spec.Template.Spec.Containers[0].Env = append(commonDeployment.Spec.Template.Spec.Containers[0].Env,
+				corev1.EnvVar{Name: "USE_ZEN", Value: isZenStr},
+				corev1.EnvVar{Name: "APP_VERSION", Value: ""},
+			)
+			needUpdate = true
+			reqLogger.Info("Adding env vars to container def: USE_ZEN and APP_VERSION", "USE_ZEN", isZenStr, "APP_VERSION", "")
+		} else if commonDeployment.Spec.Template.Spec.Containers[0].Env[22].Value != isZenStr {
+			commonDeployment.Spec.Template.Spec.Containers[0].Env[22].Value = isZenStr
+			needUpdate = true
+			reqLogger.Info("Setting container env var USE_ZEN", "USE_ZEN", isZenStr)
+		}
+
+		//Check for CLUSTER_TYPE env var
+		clusterTypeIdx := -1
+		for i := range commonDeployment.Spec.Template.Spec.Containers[0].Env {
+			if commonDeployment.Spec.Template.Spec.Containers[0].Env[i].Name == "CLUSTER_TYPE" {
+				clusterTypeIdx = i
+				break
+			}
+		}
+		if clusterTypeIdx < 0 {
+			commonDeployment.Spec.Template.Spec.Containers[0].Env = append(commonDeployment.Spec.Template.Spec.Containers[0].Env,
+				corev1.EnvVar{Name: "CLUSTER_TYPE", Value: clusterTypeStr},
+			)
+			needUpdate = true
+			reqLogger.Info("Adding env vars to container def: CLUSTER_TYPE", "CLUSTER_TYPE", clusterTypeStr)
+		} else if commonDeployment.Spec.Template.Spec.Containers[0].Env[24].Value != clusterTypeStr {
+			commonDeployment.Spec.Template.Spec.Containers[0].Env[24].Value = clusterTypeStr
+			needUpdate = true
+			reqLogger.Info("Setting container env var CLUSTER_TYPE", "CLUSTER_TYPE", isZenStr)
+		}
+
+		if needUpdate {
 			updateErr := r.Client.Update(ctx, commonDeployment)
 			if updateErr == nil {
 				reqLogger.Info("Updated common ui deployment env variable")
 			} else {
 				reqLogger.Error(updateErr, "Could not update common ui deployment env variable")
-				return updateErr
-			}
-		} else if !isZen && env == "true" {
-			reqLogger.Info("Setting use zen to false")
-			commonDeployment.Spec.Template.Spec.Containers[0].Env[22].Value = "false"
-			updateErr := r.Client.Update(ctx, commonDeployment)
-			if updateErr == nil {
-				reqLogger.Info("Updated common ui deployment env variable")
-			} else {
-				reqLogger.Error(updateErr, "Could not update common ui deployment env variable")
-				return updateErr
-			}
-		} else if !isZen && isCncf {
-			reqLogger.Info("Setting cluster type env var to cncf for non zen case")
-			envLength := len(commonDeployment.Spec.Template.Spec.Containers[0].Env)
-			if envLength > 24 {
-				commonDeployment.Spec.Template.Spec.Containers[0].Env[24].Value = clusterType
-			}
-			updateErr := r.Client.Update(ctx, commonDeployment)
-			if updateErr == nil {
-				reqLogger.Info("Updated common ui deployment with cluster type")
-			} else {
-				reqLogger.Error(updateErr, "Could not update common ui with cluster type")
 				return updateErr
 			}
 		}
+
 	} else if getError != nil && !errors.IsNotFound(getError) {
 		reqLogger.Info("Failed to get Common UI deployment")
 		return getError
