5c834b431caef8432568501ebacde844d43a7657
Steve Grube <sgrube@us.ibm.com>
2023-01-11 13:25:34 -0500

    do not update zen product configmap (#356)
    * do not update zen product configmap
    
    Signed-off-by: Steve Grube <sgrube@us.ibm.com>
    
    * remove unused func
    
    Signed-off-by: Steve Grube <sgrube@us.ibm.com>
    
    Signed-off-by: Steve Grube <sgrube@us.ibm.com>

--- controllers/commonwebuizen/commonwebuizen_controller.go
+++ controllers/commonwebuizen/commonwebuizen_controller.go
@@ -71,20 +71,6 @@ func (r *CommonWebUIZenReconciler) Reconcile(ctx context.Context, request ctrl.R
 	reqLogger.Info("In CommonWebUIZen Reconcile -- Common Services Pod namespace: " + namespace)
 	reqLogger.Info("In CommonWebUIZen Reconcile -- Operator version: " + version.Version)
 
-	//If the request is for the zen product-configmap, reconcile the adminhub values for zen
-	// if we need to create several resources, set a flag so we just requeue one time instead of after each create.
-	needToRequeue := false
-
-	if request.Name == "RECONCILE-ZEN-PRODUCT-CONFIGMAP" {
-		reqLogger.Info("Change to zen product configmap " + res.ZenProductConfigMapName + " detected - reconciling common webui updates")
-		// Check if the config maps already exist. If not, create a new one.
-		err := res.ReconcileZenProductConfigMap(ctx, r.Client, request, &needToRequeue)
-		if err != nil {
-			return ctrl.Result{}, err
-		}
-		return ctrl.Result{}, nil
-	}
-
 	// Check to see if Zen instance exists in common services namespace
 	isZen := false //ZEN Support is currently DISABLED res.IsAdminHubOnZen(ctx, r.Client, namespace)
 
@@ -164,12 +150,6 @@ func (r *CommonWebUIZenReconciler) Reconcile(ctx context.Context, request ctrl.R
 			reqLogger.Error(updateErr, "Failed updating common ui deployment")
 			return ctrl.Result{}, updateErr
 		}
-	}
-
-	if needToRequeue {
-		// one or more resources were created/updated, so requeue the request
-		reqLogger.Info("Requeue the request")
-		return ctrl.Result{Requeue: true}, nil
 	}
 
 	reqLogger.Info("COMMON UI ZEN CONTROLLER RECONCILE ALL DONE")
@@ -508,24 +488,6 @@ func zenDeploymentPredicate() predicate.Predicate {
 	}
 }
 
-func zenProductCmPredicate() predicate.Predicate {
-	namespace := os.Getenv("WATCH_NAMESPACE")
-	return predicate.Funcs{
-		UpdateFunc: func(e event.UpdateEvent) bool {
-			if e.ObjectNew.GetName() == res.ZenProductConfigMapName && e.ObjectNew.GetNamespace() == namespace {
-				return true
-			}
-			return false
-		},
-		CreateFunc: func(e event.CreateEvent) bool {
-			if e.Object.GetName() == res.ZenProductConfigMapName && e.Object.GetNamespace() == namespace {
-				return true
-			}
-			return false
-		},
-	}
-}
-
 // SetupWithManager sets up the controller with the Manager.
 func (r *CommonWebUIZenReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	// Create a new controller
@@ -535,20 +497,6 @@ func (r *CommonWebUIZenReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	}
 
 	err = c.Watch(&source.Kind{Type: &appsv1.Deployment{}}, &handler.EnqueueRequestForObject{}, zenDeploymentPredicate())
-	if err != nil {
-		return err
-	}
-
-	err = c.Watch(&source.Kind{Type: &corev1.ConfigMap{}},
-		handler.EnqueueRequestsFromMapFunc(func(a client.Object) []ctrl.Request {
-			return []ctrl.Request{
-				{NamespacedName: types.NamespacedName{
-					Name:      "RECONCILE-ZEN-PRODUCT-CONFIGMAP",
-					Namespace: a.GetNamespace(),
-				}},
-			}
-		}),
-		zenProductCmPredicate())
 	if err != nil {
 		return err
 	}
