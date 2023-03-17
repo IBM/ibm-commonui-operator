e2f644724029df98fc2c6f406dba2414b4a55c07
Steve Grube <sgrube@us.ibm.com>
2023-03-17 13:24:55 -0400

    remove iam dependency in env vars (#405)
    Signed-off-by: Steve Grube <sgrube@us.ibm.com>

--- controllers/resources/deployments.go
+++ controllers/resources/deployments.go
@@ -61,7 +61,7 @@ func getDesiredDeployment(ctx context.Context, client client.Client, instance *o
 
 	reqLogger.Info(fmt.Sprintf("Current image ID: %s", image))
 
-	volumes = append(volumes, Log4jsVolume, ClusterCaVolume, UICertVolume, InternalTLSVolume, IAMDataVolume, WebUIConfigVolume, ClusterInfoConfigVolume)
+	volumes = append(volumes, Log4jsVolume, ClusterCaVolume, UICertVolume, InternalTLSVolume, IAMDataVolume, WebUIConfigVolume, ClusterInfoConfigVolume, PlatformAuthIdpConfigVolume)
 
 	container := CommonContainer
 	container.Image = image
