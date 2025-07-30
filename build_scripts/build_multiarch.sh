736cdc42d8785703b59ca67bbeb7c608497d7508
Steve Grube <sgrube@us.ibm.com>
2025-07-30 15:15:45 -0400

    fix multiarch
    Signed-off-by: Steve Grube <sgrube@us.ibm.com>

--- build_scripts/build_multiarch.sh
+++ build_scripts/build_multiarch.sh
@@ -1,15 +1,16 @@
 echo "Pushing multi-arch image"
 
 #make docker:manifest-tool
-MANIFEST_TOOL ?= ./manifest-tool
-MANIFEST_TOOL_VERSION ?= v1.0.3
-MANIFEST_TOOL_OS ?= linux
-MANIFEST_TOOL_ARCH ?= amd64
-MANIFEST_TOOL_URL ?= https://github.com/estesp/manifest-tool/releases/download/$(MANIFEST_TOOL_VERSION)/manifest-tool-$(MANIFEST_TOOL_OS)-$(MANIFEST_TOOL_ARCH)
+export MANIFEST_TOOL ?= ./manifest-tool
+export MANIFEST_TOOL_VERSION ?= v1.0.3
+export MANIFEST_TOOL_OS ?= linux
+export MANIFEST_TOOL_ARCH ?= amd64
+export MANIFEST_TOOL_URL ?= https://github.com/estesp/manifest-tool/releases/download/$(MANIFEST_TOOL_VERSION)/manifest-tool-$(MANIFEST_TOOL_OS)-$(MANIFEST_TOOL_ARCH)
 
 echo "Installing manifest-tool $(MANIFEST_TOOL_VERSION) ($(MANIFEST_TOOL_OS)-$(MANIFEST_TOOL_ARCH)) from $(MANIFEST_TOOL_URL)" && \
-                curl '-#' -fL -o $(MANIFEST_TOOL) $(MANIFEST_TOOL_URL) && \
-                chmod +x $(MANIFEST_TOOL) \
+curl '-#' -fL -o $(MANIFEST_TOOL) $(MANIFEST_TOOL_URL) && \
+chmod +x $(MANIFEST_TOOL)
+
 $(MANIFEST_TOOL) --version
 
 ## Push the manifest to a Docker registry
